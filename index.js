b53d24e9d3558195ec1e0de30d72d19b90f1e5ae
angelocapone1980 <angcap@hotmail.it>
2020-12-07 16:34:07 +0100

    test stack

--- index.js
+++ index.js
@@ -14,13 +14,41 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "mysql",
   user: "root",
-  password: "secret",
-  database: "todos"
+  password: "secret"
 });
 
 con.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
+});
+
+app.post('/mostra', cors(), function (request, response) {
+  if (request.method.toLowerCase() == 'post') {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    var sql = "SELECT * FROM test.books";
+    con.query(sql, function (err, result, fields) {
+      let tabella = "<table>";
+      tabella += "<tr>";
+      if (err) throw err;
+      for (field of fields) {
+        if (field.name != "id")
+          tabella += "<th>" + field.name + "</th>";
+      }
+      tabella += "</tr>";
+      for (var i = 0; i < result.length; i++) {
+        tabella += "<tr>";
+        tabella += "<td>" + result[i].title + "</td>";
+        tabella += "<td>" + result[i].author + "</td>";
+        tabella += "<td>" + result[i].price + "</td>";
+        tabella += "<td>" + result[i].nump + "</td>";
+        tabella += "</tr>";
+      }
+      tabella += "</table>";
+      response.end(tabella);
+    });
+  }
 });
 
 var formidable = require('formidable');
@@ -35,7 +63,32 @@ app.post('/inserisci', cors(), function (request, response) {
       response.header("Access-Control-Allow-Origin", "*");
       response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       response.writeHead(200, { 'Content-Type': 'application/json' });
-      var sql = "INSERT INTO books (title, author, price, nump) VALUES ('";
+      var sqltest = "select count(*) as conta from information_schema.tables where table_schema = 'test' and table_name = 'books'";
+      let conta = 0;
+      con.query(sqltest, function (err, result, fields) {
+          if (err) throw err;
+          for (var i = 0; i < result.length; i++) {
+            conta = parseInt(result[i].conta);
+          }
+        });
+      if (conta == 0) {
+        var sqlcreateschema = "CREATE SCHEMA test";
+        con.query(sqlcreateschema, function (err, result) {
+          if (err) throw err;
+        });
+
+        var sqlcreatetable = "CREATE TABLE test.books (";
+        sqlcreatetable+= "id int(11) NOT NULL AUTO_INCREMENT,";
+        sqlcreatetable+= "title varchar(50) DEFAULT NULL,";
+        sqlcreatetable+= "author varchar(50) DEFAULT NULL,";
+        sqlcreatetable+= "price double DEFAULT NULL,";
+        sqlcreatetable+= "nump int(11) DEFAULT NULL,";
+        sqlcreatetable+= "PRIMARY KEY (id))";
+        con.query(sqlcreatetable, function (err, result) {
+          if (err) throw err;
+        });
+      }
+      var sql = "INSERT INTO test.books (title, author, price, nump) VALUES ('";
       sql += title + "','";
       sql += author + "',";
       sql += price + ",";
@@ -51,11 +104,3 @@ app.post('/inserisci', cors(), function (request, response) {
 var listener = app.listen(process.env.PORT || 8080, function () {
   console.log('listening on port ' + listener.address().port);
 });
-
-/*   con.connect(function(err) {
-    if (err) throw err;
-    con.query("SELECT name, address FROM customers", function (err, result, fields) {
-      if (err) throw err;
-      console.log(fields);
-    });
-  }); */
